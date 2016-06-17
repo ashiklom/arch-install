f5ce59537db115d858922ef74064792778b12a48
Alexey Shiklomanov <ashiklom@bu.edu>
2016-06-17 01:27:25 -0400

    Fix typo in partition windows2

--- partition.windows2.sh
+++ partition.windows2.sh
@@ -27,9 +27,9 @@ sleep 2
 
 # Mount partitions
 echo "Mounting partitions"
-mount /dev/sda6 /mnt
+mount /dev/sda5 /mnt
 mkdir /mnt/home /mnt/boot
-mount /dev/sda6 /mnt/home
+mount /dev/sda5 /mnt/home
 mount /dev/sda2 /mnt/boot
 
 sleep 2
